85bc2f7d36435784f67f4393b4f8bb7a1c8663ef
Matilda-bit <polinaovras@gmail.com>
2024-04-16 17:08:23 +0300

    added a game board

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    function handleSelectSquare(rowIndex, colIndex, playerSymbol) {
+        setGameBoard((prevGameBoard) => {
+            const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updateBoard[rowIndex][colIndex] = playerSymbol;
+            return updateBoard;
+        });
+
+    }
+
+
+    return (
+        <ol id="game-board">
+           {gameBoard.map((row, rowIndex) => (
+           <li key={rowIndex}>
+            <ol>
+                {row.map((playerSymbol,colIndex) => (
+                    <li key={colIndex}>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex, playerSymbol)}>{playerSymbol}</button>
+                    </li>))}
+            </ol>
+           </li>))}       
+        </ol>);
+}
